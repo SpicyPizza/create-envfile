7a504ef28fe2f82cf18d105e08ab2db5633067f2
Forest Anderson <forestkzanderson@gmail.com>
2019-08-20 20:02:19 -0400

    Added basic envfile creator

---
+++ src/create-envfile.py
@@ -0,0 +1,12 @@
+import os
+
+env_keys = list(dict(os.environ).keys())
+
+out_file = ""
+
+for key in env_keys:
+    if key.startswith("INPUT_ENVKEY_"):
+        out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
+
+with open(os.environ.get("INPUT_FILE_NAME"), "w") as text_file:
+    text_file.write(out_file)
