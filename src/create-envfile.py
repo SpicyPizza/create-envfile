fbf2ed35756e16c31ea9ed8015ddedae490f985a
Samuel <knipknap@gmail.com>
2020-06-26 00:16:30 +0200

    Update create-envfile.py

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -6,7 +6,7 @@ out_file = ""
 
 for key in env_keys:
     if key.startswith("INPUT_ENVKEY_"):
-        out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
+        out_file += key.split("INPUT_ENVKEY_")[1] + "='" + os.environ.get(key) + "'\n"
 
 with open("/github/workspace/" + str(os.environ.get("INPUT_FILE_NAME")), "w") as text_file:
     text_file.write(out_file)
