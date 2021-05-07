beb6b695ed086e455245027510154e318c39f43c
Forest Anderson <forestkzanderson@gmail.com>
2021-05-07 16:40:58 -0400

    Added default directory envkey option

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -9,15 +9,15 @@ for key in env_keys:
         out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
 
 # get directory name in which we want to create .env file
-directory = str(os.environ.get("INPUT_DIRECTORY"))
+directory = str(os.environ.get("INPUT_DIRECTORY", ""))
 
 # get file name in which we want to add variables
 # .env is set by default
 file_name = str(os.environ.get("INPUT_FILE_NAME"))
 
-path = str(os.environ.get("INPUT_GITHUB_WORKSPACE"))
+path = str(os.environ.get("GITHUB_WORKSPACE", ""))
 if path == "":
-    raise Exception("Could not get the GITHUB_WORKSPACE environment variable.") 
+    raise Exception("Could not get the GITHUB_WORKSPACE environment variable.")
 
 with open(os.path.join(path, directory, file_name), "w") as text_file:
     text_file.write(out_file)
