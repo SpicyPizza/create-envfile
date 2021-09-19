42f7f69a01125a193d1e615c312582b7b16a4765
KonstantinKlepikov <oformleno@gmail.com>
2021-09-19 22:54:07 +0200

    typo

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -15,7 +15,5 @@ directory = str(os.environ.get("INPUT_DIRECTORY", ""))
 # .env is set by default
 file_name = str(os.environ.get("INPUT_FILE_NAME"))
 
-path = "."
-
-with open(os.path.join(path, directory, file_name), "w") as text_file:
+with open(os.path.join(".", directory, file_name), "w") as text_file:
     text_file.write(out_file)
